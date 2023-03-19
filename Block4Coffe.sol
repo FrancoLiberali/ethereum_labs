473197e3a319627f2f20769f24514f38c2b776a6
Franco Liberali <franco.liberali@gmail.com>
2023-03-19 18:03:21 +0100

    add coffee providers

--- Block4Coffe.sol
+++ Block4Coffe.sol
@@ -6,7 +6,16 @@ pragma abicoder v2;
 
 contract Block4Coffee {
     address owner = msg.sender;
-    uint sellingPrice = 1 * (1 gwei);
+    uint public sellingPrice = 1 * (1 gwei);
+    address[] coffeeProviders;
+
+    function addCoffeeProvider(address newCoffeeProvider) external {
+        require(msg.sender == owner);
+
+        if (!member(newCoffeeProvider, coffeeProviders)) {
+            coffeeProviders.push(newCoffeeProvider);
+        }
+    }
 
     function fixCoffeePrice(uint newPrice) external {
         require(msg.sender == owner);
@@ -21,5 +30,15 @@ contract Block4Coffee {
     function kill() external{
         require(msg.sender == owner);
         selfdestruct(payable(msg.sender));
+    }
+
+    function member(address s, address[] memory tab) pure private returns(bool) {
+        uint length = tab.length;
+
+        for (uint i=0; i<length; i++) {
+            if (tab[i] == s) return true;
+        }
+
+        return false;
     }
 }
